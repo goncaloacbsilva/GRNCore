78ed7e921170fde5134841280e22bc7f3cb9f849
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-02-16 19:17:21 +0000

    feat: implement sidebar component

---
+++ src/components/sidebar/data.ts
@@ -0,0 +1,23 @@
+import { Dna, HardDrive, UsersRound } from "lucide-react";
+import type { NavigationItem } from "./nav-main";
+
+export const NAVIGATION_ITEMS: NavigationItem[] = [
+  {
+    title: "Models",
+    url: "#",
+    icon: Dna,
+    isActive: true,
+    items: [
+      {
+        title: "Local",
+        url: "#",
+        icon: HardDrive,
+      },
+      {
+        title: "Community",
+        url: "#",
+        icon: UsersRound,
+      },
+    ],
+  },
+];
