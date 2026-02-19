bf849208a9115a93ebf3a606df881f566f00d9f6
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-02-19 15:16:47 +0000

    feat(editor/overlay/toolbar): create add node/edge buttons

---
+++ src/components/views/editor/overlay/toolbar/components/add-edge-toggle.tsx
@@ -0,0 +1,16 @@
+import { Toggle } from '@/components/ui/toggle'
+import { MousePointerClick } from 'lucide-react'
+
+export function AddEdgeToggle() {
+    return (
+        <Toggle
+            className="rounded-full text-sm data-[state=on]:border-2 cursor-pointer text-muted-foreground bg-white px-4 hover:border-[#3b83f682] hover:text-[#3B82F6] hover:bg-white data-[state=on]:border-[#3b83f6d9] data-[state=on]:text-[#3B82F6] data-[state=on]:bg-white  transition-all"
+            aria-label="Toggle bookmark"
+            size="default"
+            variant="outline"
+        >
+            Add Edge
+            <MousePointerClick />
+        </Toggle>
+    )
+}
