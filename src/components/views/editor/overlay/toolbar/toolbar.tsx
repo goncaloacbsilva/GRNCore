667026dcb8449430bae5d8a01ce8e124c4f36acc
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-02-19 15:17:34 +0000

    feat(overlay): create toolbar

---
+++ src/components/views/editor/overlay/toolbar/toolbar.tsx
@@ -0,0 +1,20 @@
+import { Menubar } from '@/components/ui/menubar'
+import { AddEdgeToggle } from './components/add-edge-toggle'
+import { AddNodeButton } from './components/add-node-button'
+import { EditMenu, FileMenu, ViewMenu } from './components/menus'
+
+export function Toolbar() {
+    return (
+        <div className="absolute top-5 left-5 flex flex-row items-center gap-8">
+            <Menubar>
+                <FileMenu />
+                <EditMenu />
+                <ViewMenu />
+            </Menubar>
+            <div className="flex flex-row items-center gap-4">
+                <AddNodeButton />
+                <AddEdgeToggle />
+            </div>
+        </div>
+    )
+}
