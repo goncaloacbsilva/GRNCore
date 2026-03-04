6f6142e82020d81223cd33c602b7d46649434b70
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-03-04 17:52:01 +0000

    feat(store): add connect mode to editor state

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -1,20 +1,39 @@
+import { toast } from 'sonner'
 import { create } from 'zustand'
 import { combine } from 'zustand/middleware'
 
 interface EditorState {
     addNodeDialogVisible: boolean
+    connectModeEnabled: boolean
 
     setAddNodeDialogVisible: (visible: boolean) => void
+    setConnectMode: (enabled: boolean) => void
 }
 
 export const useEditorStore = create<EditorState>()(
     combine(
         {
             addNodeDialogVisible: false,
+            connectModeEnabled: false,
         },
         (set) => ({
-            setAddNodeDialogVisible: (visible: boolean) =>
+            setAddNodeDialogVisible: (visible) =>
                 set(() => ({ addNodeDialogVisible: visible })),
+            setConnectMode: (enabled) => {
+                if (enabled) {
+                    toast.info('Switched to Connect Mode', {
+                        position: 'top-right',
+                        description:
+                            'Edges can be created between nodes until this mode gets disabled',
+                    })
+                } else {
+                    toast.info('Switched to Default Mode', {
+                        position: 'top-right',
+                    })
+                }
+
+                set(() => ({ connectModeEnabled: enabled }))
+            },
         })
     )
 )
