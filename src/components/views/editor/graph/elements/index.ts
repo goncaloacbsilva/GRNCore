b5b8352711a6edec08bacf782ecfb12cfa356ef9
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-03-04 17:54:16 +0000

    feat: implement connect mode (add edges)

--- src/components/views/editor/graph/elements/index.ts
+++ src/components/views/editor/graph/elements/index.ts
@@ -1,3 +1,4 @@
 export { default as RegulatoryNode } from './regulatory-node'
 export * from './regulatory-edge'
 export * from './regulatory-edge-marker'
+export * from './connection-line'
--- src/components/views/editor/graph/elements/regulatory-node.tsx
+++ src/components/views/editor/graph/elements/regulatory-node.tsx
@@ -10,6 +10,8 @@ import {
 import type { RegulatoryNodeProperties } from '@/lib/schema'
 import { DEFAULT_NODE_HEIGHT } from '../config'
 import { getNodeContentMinWidth } from '../utils'
+import { useEditorStore } from '@/store'
+import { twJoin } from 'tailwind-merge'
 
 const RegulatoryNode = ({
     id,
@@ -18,13 +20,24 @@ const RegulatoryNode = ({
 }: NodeProps<Node<RegulatoryNodeProperties>>) => {
     const connection = useConnection()
     const contentMinWidth = getNodeContentMinWidth(data.name)
+    const connectModeActive = useEditorStore(
+        (state) => state.connectModeEnabled
+    )
 
     const isTarget = connection.inProgress && connection.fromNode.id !== id
 
-    const connectModeActive = false
-
     return (
-        <div className="h-full px-2 py-2 flex flex-col items-center justify-center bg-white border-2 border-[#E2E8F0] rounded-sm text-sm">
+        <div
+            className={twJoin(
+                'h-full px-2 py-2 flex flex-col items-center justify-center bg-white border-2 rounded-sm text-sm',
+                connectModeActive
+                    ? 'group border-[#e2e8f098] hover:border-[#3b83f6d9] transition-all'
+                    : 'border-[#E2E8F0]',
+                connection.inProgress &&
+                    connection.fromNode.id === id &&
+                    'border-[#3b83f6d9]!'
+            )}
+        >
             <NodeResizer
                 color="#2f81ed"
                 handleStyle={{
@@ -53,7 +66,19 @@ const RegulatoryNode = ({
                     isConnectableStart={false}
                 />
             )}
-            <div className="whitespace-nowrap">{data.name}</div>
+            <div
+                className={twJoin(
+                    'whitespace-nowrap',
+                    connectModeActive &&
+                        !(
+                            connection.inProgress &&
+                            connection.fromNode.id === id
+                        ) &&
+                        'text-[#0a0a0a51] group-hover:text-[#0a0a0a]'
+                )}
+            >
+                {data.name}
+            </div>
         </div>
     )
 }
