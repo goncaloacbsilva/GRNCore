7d6be28d7bce7ffe343d9adfb686797f0a34a84f
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-03-23 01:16:20 +0000

    feat(hooks): support windows 'delete' shortcut

--- src/hooks/use-hotkeys-setup.ts
+++ src/hooks/use-hotkeys-setup.ts
@@ -1,11 +1,16 @@
-import { useHotkeys } from 'react-hotkeys-hook'
+import { useHotkeys, useRecordHotkeys } from 'react-hotkeys-hook'
 import { useElementsActions } from './use-elements-actions'
 import { useEditorStore } from '@/store'
 import { useShallow } from 'zustand/react/shallow'
 
 export function useHotkeysSetup() {
-    const { copyAction, pasteAction, duplicateAction, cutAction } =
-        useElementsActions()
+    const {
+        copyAction,
+        pasteAction,
+        duplicateAction,
+        cutAction,
+        deleteAction,
+    } = useElementsActions()
     const { setAddNodeDialogVisible, connectModeEnabled, setConnectMode } =
         useEditorStore(
             useShallow((state) => ({
@@ -15,7 +20,10 @@ export function useHotkeysSetup() {
             }))
         )
 
-    // Shortcuts are defined here
+    // Support Windows delete
+    useHotkeys('delete', () => deleteAction(), {
+        preventDefault: true,
+    })
 
     useHotkeys('mod+d', () => duplicateAction(), {
         preventDefault: true,
