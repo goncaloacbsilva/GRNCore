7ad596b861f0bfb943ea6859a058941f7b4ee0df
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2026-04-20 23:21:45 +0100

    feat(lib): add regulatory node rules

--- src/lib/schema/regulatory-node-properties.ts
+++ src/lib/schema/regulatory-node-properties.ts
@@ -1,5 +1,14 @@
 import * as z from 'zod'
 
+export const RegulatoryNodeRuleSchema = z.object({
+    id: z.string(),
+
+    // Target level if rule is satisfied
+    target: z.int().positive().max(9).min(1),
+
+    expression: z.string().nonempty({ message: 'Expression cannot be empty' }),
+})
+
 export const RegulatoryNodePropertiesSchema = z.object({
     // Name of the Gene
     name: z
@@ -12,8 +21,12 @@ export const RegulatoryNodePropertiesSchema = z.object({
 
     // Whether the node acts as an external input
     isInputNode: z.boolean().default(false),
+
+    // Logical rules
+    rules: z.array(RegulatoryNodeRuleSchema).default([]),
 })
 
-export type RegulatoryNodeProperties = z.input<
+export type RegulatoryNodeProperties = z.infer<
     typeof RegulatoryNodePropertiesSchema
 >
+export type RegulatoryNodeRule = z.infer<typeof RegulatoryNodeRuleSchema>
